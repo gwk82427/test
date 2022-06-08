72f2030e78123a80bc65c63e6d1a5d9ede84d9a6
Muthu <MaheshSenthilKumar.Muthu@igt.com>
2022-06-08 16:07:51 +0530

    Initial Commit

---
+++ Project_History_2010.ps1
@@ -0,0 +1,37 @@
+﻿$repoResult = Invoke-RestMethod "http://rnotfsat:8080/tfs/_apis/git/repositories?api-version=1.0" -Method Get -UseDefaultCredentials
+
+$releases = @()
+
+$repoResult.value | % {
+
+    $project = $_.name
+    $repo = $_.id
+
+    Write-Host $project $repo  
+
+   $url = "http://rnotfsat:8080/tfs/_apis/git/repositories/$repo/commits?searchCriteria.fromDate=1/1/2021&searchCriteria.toDate=12/31/2021"
+
+    $result = Invoke-RestMethod $url -Method Get -UseDefaultCredentials
+
+    if ($result.count -gt 0){
+        
+        $result.value | % {
+            
+            $releases += [pscustomobject]@{ 
+                Project = $project 
+                #Branch = $_.name 
+                #RefId = $_.objectId 
+                Commit=$_.committer.name
+                Email=$_.committer.email
+                Date=$_.committer.date
+                Comment=$_.comment
+
+               
+            }
+        }
+    }
+
+}
+
+#$releases | Export-csv -Path D:\2017\2017-TFSProject_History.csv |Sort-Object -Property Project | Format-Table -AutoSize
+$releases | Sort-Object -Property Project | Format-Table -AutoSize
---
+++ Project_branches_2010.ps1
@@ -0,0 +1,32 @@
+﻿$repoResult = Invoke-RestMethod "http://rnotfsat:8080/tfs/_apis/git/repositories?api-version=1.0" -Method Get -UseDefaultCredentials
+
+$releases = @()
+
+$repoResult.value | % {
+
+    $project = $_.name
+    $repo = $_.id
+    #Write-Host $project $repo  
+
+    #Invoke-RestMethod "http://rnotfsat:8080/tfs/_apis/git/repositories/fb13e7b7-d47e-43b7-bc7f-b3db0ab91338/refs?api-version=2.0" -Method Get -UseDefaultCredentials |ConvertTo-Json -Depth 10
+
+   $url = "http://rnotfsat:8080/tfs/_apis/git/repositories/$repo/refs?api-version=2.0"
+
+    $result = Invoke-RestMethod $url -Method Get -UseDefaultCredentials
+
+    if ($result.count -gt 0){
+        
+        $result.value | % {
+            
+            $releases += [pscustomobject]@{ 
+                Project = $project 
+                Branch = $_.name 
+                RefId = $_.objectId 
+            }
+        }
+    }
+
+}
+
+#$releases | Export-csv -Path D:\2010\Project_Branches.csv |Sort-Object -Property Project | Format-Table -AutoSize
+$releases | Sort-Object -Property Project | Format-Table -AutoSize
